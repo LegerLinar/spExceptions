83e531646fd0fbc29b5d5dc7746514930b9031c4
pavelbesedin <pavelbesedin97@mail.ru>
2023-05-02 04:24:39 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Password.passwordCreate("SuperNagibator777", "EjikVtumane_123", "EjikVtumane_123");
+
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Login может содержать в себе только латинские буквы, цифры и знак подчеркивания");
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Password может содержать в себе только латинские буквы, цифры и знак подчеркивания");
+        }
+
     }
+
+
 }
+
+//1. Напишите статический метод, который принимает на вход три параметра: login, password и confirmPassword.
+//2. Параметр Login содержит в себе только латинские буквы, цифры и знак подчеркивания.
+//*Например, java_skypro_go*
+//    1. У параметра login есть ограничение по длине – он должен быть равен или меньше 20 символов.
+//    Если login длиннее 20 символов, то  необходимо выбросить исключение – `WrongLoginException`.
+//3. Параметр Password содержит в себе только латинские буквы, цифры и знак подчеркивания.
+//*Например, D_1hWiKjjP_9*
+//4. У параметра password тоже есть ограничение по длине – он должен быть строго меньше 20 символов. 
+//5. Параметры password и confirmPassword должны быть равны. Если это требование не соблюдается, то нужно выбросить `WrongPasswordException`.
+//6. Для обработки исключений используйте multi-catch block.
+//7. Метод ничего не должен возвращать , если значения параметров подходят под ограничения,  или бросать исключение, если условия не выполняются.
+//8. При вызове метода необходимо обработать потенциальные исключения
